dfbf377399bd1469fe942462be4f2b6c8dbfe510
ap <abhayprasadnls@gmail.com>
2026-03-04 17:10:28 +0000

    fix radio selector issues

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { fromatCurency } from './utils/money.js';
 
 let productsHtml = '';
 
@@ -25,7 +26,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${fromatCurency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ scripts/utils/money.js
@@ -0,0 +1,3 @@
+export function fromatCurency(cents){
+   return (cents / 100).toFixed(2)
+}
